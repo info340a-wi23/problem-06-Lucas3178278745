21b23aebe663863a6f465ac03bf54d1e70ee7df9
Lucas3178278745 <muyangzhou8@gmail.com>
2023-02-16 00:51:54 -0800

    Revert "pass all tests"
    This reverts commit cf33010e3de44c3c35d48cefc74f9c67e808d552.

--- problem-c/js/index.js
+++ problem-c/js/index.js
@@ -1,5 +1,4 @@
 'use strict';
 
-import { renderTaskView } from "./Controller.js";
-
-renderTaskView();
+//you can delete the below line of code
+console.log("If you see this message, the script has loaded!")
